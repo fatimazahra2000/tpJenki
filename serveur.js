2ed69f5ec043d7e597fb5545e4728ff89d4bdc39
fatima zahra <fatimazahraablou@gmail.com>
2026-03-10 13:44:32 +0000

    fix test script

---
+++ serveur.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Calculatrice Node.js fonctionne !");
+});
+
+app.listen(3000, () => {
+    console.log("Serveur lancé sur port 3000");
+});
